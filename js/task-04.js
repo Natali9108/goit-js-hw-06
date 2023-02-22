8f122e1781ad5a613ba684adce547e213ebccb7a
Natali <114168847+Natali9108@users.noreply.github.com>
2023-02-22 21:55:12 +0200

    Update task-04

--- js/task-04.js
+++ js/task-04.js
@@ -1,34 +1,18 @@
-// Створи змінну counterValue, в якій буде зберігатися
-//поточне значення лічильника та ініціалізуй її значенням 0.
-// Додай слухачів кліків до кнопок, всередині яких
-//збільшуй або зменшуй значення лічильника.
-// Оновлюй інтерфейс новим значенням змінної counterValue.
-
 const decrementBtn = document.querySelector(`button[data-action="decrement"]`);
-console.log(decrementBtn);
 const value = document.querySelector("#value");
-console.log(value);
 const incrementBtn = document.querySelector(`button[data-action="increment"]`);
-console.log(incrementBtn);
-
-decrementBtn.addEventListener("click", decrementValue);
-incrementBtn.addEventListener("click", incrementValue);
 
 const counter = {
 	counterValue: 0,
 	decrement() {
 		this.counterValue -= 1;
+		value.textContent = counter.counterValue;
 	},
 	increment() {
 		this.counterValue += 1;
+		value.textContent = counter.counterValue;
 	},
 };
 
-function decrementValue() {
-	counter.decrement();
-	return (value.textContent = counter.counterValue);
-}
-function incrementValue() {
-	counter.increment();
-	return (value.textContent = counter.counterValue);
-}
+decrementBtn.addEventListener("click", counter.decrement.bind(counter));
+incrementBtn.addEventListener("click", counter.increment.bind(counter));
