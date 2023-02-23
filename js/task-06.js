c125e52b08bf694bde18c218be91ddf2f3827a88
Natali <114168847+Natali9108@users.noreply.github.com>
2023-02-23 18:17:26 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -2,9 +2,13 @@ const input = document.querySelector("#validation-input");
 
 input.addEventListener("blur", onBlurInput);
 
-function onBlurInput() {
-	if (input.value.length === Number(input.dataset.length)) {
-		input.classList.add("valid");
-		input.classList.remove("invalid");
-	} else input.classList.remove("valid"), input.classList.add("invalid");
+function onBlurInput(event) {
+	const symbolLenght = event.currentTarget.value.length;
+	const defaultValue = Number(event.currentTarget.dataset.length);
+	if (symbolLenght === defaultValue) {
+		event.currentTarget.classList.add("valid");
+		event.currentTarget.classList.remove("invalid");
+	} else
+		event.currentTarget.classList.remove("valid"),
+			event.currentTarget.classList.add("invalid");
 }
