c597dd36ba744fc87c634ca4847d9b9a42c5a363
Natali <114168847+Natali9108@users.noreply.github.com>
2023-02-24 15:37:44 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,17 @@
+const refs = {
+	spanText: document.querySelector(".color"),
+	changeColorBtn: document.querySelector(".change-color"),
+};
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+	return `#${Math.floor(Math.random() * 16777215)
+		.toString(16)
+		.padStart(6, 0)}`;
 }
+
+const handelChangeBackgroundColorBody = (event) => {
+	document.body.style.backgroundColor = getRandomHexColor();
+	refs.spanText.textContent = getRandomHexColor();
+};
+
+refs.changeColorBtn.addEventListener("click", handelChangeBackgroundColorBody);
